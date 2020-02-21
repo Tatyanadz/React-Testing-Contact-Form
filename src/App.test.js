ad7acf98889bce08266f2c295cde4fb329ef9758
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-02-20 19:02:27 -0500

    in process

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import 'mutationobserver-shim';
 import App from "./App";
 
 test("renders App without crashing", () => {
